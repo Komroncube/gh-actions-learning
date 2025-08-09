2e9c0836e4137aaa813676925f0c776de812b06d
Abdujabborov Kamronbek <kamronbek_abdujabborov@epam.com>
2025-08-09 18:08:36 +0500

    fix testing

--- src/arithmetics.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def add(a: int, b: int):
-    return np.add(a, b)
-
-def subtract(a: int, b: int):
-    return np.subtract(a, b)
---
+++ tests/calculator_tests.py
@@ -0,0 +1,8 @@
+from src.calculator import add, subtract
+import pytest
+
+def test_add():
+    assert add(1, 2) == 3
+
+def test_subtract():
+    assert subtract(5, 3) == 2
